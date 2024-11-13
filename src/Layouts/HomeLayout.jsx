7d0e25bfccc019d8adff75f405cc59b16493cddf
Abubokkor98 <mail.abubokkor@gmail.com>
2024-11-13 22:08:53 +0600

    Router Setup and folder structure analysis

---
+++ src/Layouts/HomeLayout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../Components/Header'
+
+export default function HomeLayout() {
+  return (
+    <div>
+        <Header></Header>
+        {/* nav */}
+        {/* main */}
+    </div>
+  )
+}
